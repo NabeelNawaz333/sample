b6ea0a13af31faa70356e11801a7f038efd5c259
Shehzad Ahmed <hshehzad820@gmail.com>
2024-11-18 18:53:25 +0500

    change-name

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hello shehzad</h1>
+        <h2>Hello nabeel</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id commodi natus, sed qui laboriosam asperiores nobis quo ab aliquam, eos vel ratione obcaecati reiciendis aliquid dolorum nostrum non voluptatibus ad!</p>
       </div>
     </>
