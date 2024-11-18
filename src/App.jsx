08b29d0515e28cba110794fa6277ecc413d52bc4
Shehzad Ahmed <hshehzad820@gmail.com>
2024-11-18 17:25:22 +0500

    nothing3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  
   return (
     <>
       <div>
         <h1>Hello Nabeel</h1>
       </div>
-       
     </>
-  )
+  );
 }
-export default App
+export default App;
